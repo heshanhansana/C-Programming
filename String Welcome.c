2c56e1778af10181b3e4471b643ec8475399ac24
Heshan Hansana <heshanhansana247@gmail.com>
2024-08-09 18:13:10 +0530

    Create String Welcome.c
    Exercise 1 part 1

---
+++ String Welcome.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello,Welcome\n\n");
+    printf("My Personal Details\n");
+    printf("-------------------\n");
+    printf("Name:Saman\n");
+    printf("Home Town:Nugegoda\n");
+    printf("First Year");
+    return 0;
+}
